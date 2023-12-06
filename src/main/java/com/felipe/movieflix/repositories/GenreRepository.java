b3000dfe2a8c21848b55f3bd6b625551f784b2a8
felipeam10 <felipeam10@hotmail.com>
2023-12-06 14:03:28 -0300

    findAll - genre

---
+++ src/main/java/com/felipe/movieflix/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.felipe.movieflix.repositories;
+
+import com.felipe.movieflix.entities.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
